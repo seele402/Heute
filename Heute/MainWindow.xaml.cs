cabcd8f68ab4d33b83199d963793b077635c6e77
veselv2010 <32619716+veselv2010@users.noreply.github.com>
2018-12-23 14:15:32 +0300

    Fix #1 + Fix #5

--- Heute/MainWindow.xaml.cs
+++ Heute/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using VkNet;
 using VkNet.Model;
 using System.Windows.Media;
 using System.Windows.Input;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Heute
 {
@@ -11,37 +13,29 @@ namespace Heute
     /// </summary>
     public partial class MainWindow : Window
     {
-        
-        Thickness LeftSide = new Thickness(-110.8, 0, 0, 0);
-        Thickness RightSide = new Thickness(0, 0, -110.8, 0);
-        SolidColorBrush Off = new SolidColorBrush(Color.FromArgb(100, 244, 244, 245));
-        SolidColorBrush On = new SolidColorBrush(Color.FromArgb(100, 68, 148, 74));
-
-        public bool Toggled1 { get; set; } = false;
-
         public MainWindow()
         {
             InitializeComponent();
-            Back.Fill = Off;
-            Toggled1 = false;
-            Dot.Margin = LeftSide;
         }
-        
-        // Работа слайдера (нажатие на круг)
-        private void Ellipse_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (!Toggled1) // Слайдер активирован
-            {
-                Back.Fill = On;
-                Toggled1 = true;
-                Dot.Margin = RightSide;
 
-                int Hour = 00;
-                int Minute = 00;
-                int Seconds = 00;
+        // Работа слайдера
+        private async void Switch_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            int Hour = 0;
+            int Minute = 0;
+            int Seconds = 0;
+
+            if (!this.Switch.Toggled)
+            {
+                //this._cancellationGetUpdates?.Cancel();
+                return;
+            }
+
+            await Task.Factory.StartNew(() =>
+            {                           
                 while (true)
                 {
-                    System.Threading.Thread.Sleep(1000);
+                    Thread.Sleep(1000);
 
                     if ((Hour == System.DateTime.Now.Hour) && (Minute == System.DateTime.Now.Minute) && (Seconds == System.DateTime.Now.Second))
                     {
@@ -59,54 +53,12 @@ namespace Heute
                         });
                     }
                 }
-            }
-            else // Слайдер не активирован
-            {
-                Back.Fill = Off;
-                Toggled1 = false;
-                Dot.Margin = LeftSide;
-            }
+            });
         }
-        // Работа слайдера(нажатие на бекграунд слайдера)
-        private void Back_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+
+        private void Switch_Loaded(object sender, RoutedEventArgs e)
         {
-            if (!Toggled1) // Слайдер активирован
-            {
-                Back.Fill = On;
-                Toggled1 = true;
-                Dot.Margin = RightSide;
-
-                int Hour = 00;
-                int Minute = 00;
-                int Seconds = 00;
-                while (true)
-                {
-                    System.Threading.Thread.Sleep(1000);
-
-                    if ((Hour == System.DateTime.Now.Hour) && (Minute == System.DateTime.Now.Minute) && (Seconds == System.DateTime.Now.Second))
-                    {
-                        var api = new VkApi();
-                        // авторизация
-                        api.Authorize(new ApiAuthParams
-                        {
-                            AccessToken = "access_token"
-                        });
-                        // отправка сообщения
-                        api.Messages.Send(new VkNet.Model.RequestParams.MessagesSendParams
-                        {
-                            PeerId = 2000000131,
-                            Message = "а"
-                        });
-                    }
-                    
-                }
-            }
-            else // Слайдер не активирован
-            {
-                Back.Fill = Off;
-                Toggled1 = false;
-                Dot.Margin = LeftSide;
-            }
+            Switch.TurnOff();
         }
     }
 }
