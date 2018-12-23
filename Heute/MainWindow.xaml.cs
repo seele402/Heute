2fbb07c7743b228c4f4f7b40a019a0e535eb4f4b
JigiDo <JigiDo@outlook.com>
2018-12-23 17:45:11 +0200

    Fix #12 Авторизация вынесена за пределы свитча

--- Heute/MainWindow.xaml.cs
+++ Heute/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using System.Windows;
 using VkNet;
 using VkNet.Model;
-using System.Windows.Media;
 using System.Windows.Input;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,6 +17,14 @@ namespace Heute
             InitializeComponent();
             Switch.TurnOff();
         }
+        private void Token()
+        {
+            var api = new VkApi();
+            api.Authorize(new ApiAuthParams
+            {
+                AccessToken = ""
+            });
+        }
 
         // Работа слайдера
         private async void Switch_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -32,24 +39,19 @@ namespace Heute
             if (this.Switch.Toggled == true)
             {
                 await Task.Factory.StartNew(() =>
-                {
-                    var api = new VkApi();
-                    api.Authorize(new ApiAuthParams
-                    {
-                        AccessToken = ""
-                    });
-
+                {                    
                     while (true)
                     {
 
                     Thread.Sleep(1000);
                         
-                    if ((System.DateTime.Now.Hour == 17) && (System.DateTime.Now.Minute == 42) && (System.DateTime.Now.Second == 50))
+                    if ((System.DateTime.Now.Hour == 17) && (System.DateTime.Now.Minute == 42) && (System.DateTime.Now.Second == 00))
                     {
                             // отправка сообщения
+                            var api = new VkApi();
                             api.Messages.Send(new VkNet.Model.RequestParams.MessagesSendParams
                             {
-                                PeerId = 53658629,
+                                PeerId = 133199090,
                                 Message = "а"
                             });
                         }
