6399bd1e012fe977be91ba83466964d182a78e15
JigiDo <JigiDo@outlook.com>
2019-01-03 19:58:38 +0200

    Fix #12 Fix #7

---
+++ Heute/APIwk.cs
@@ -0,0 +1,25 @@
+﻿using VkNet;
+using VkNet.Model;
+
+namespace Heute
+{
+    class APIwk
+    {
+        static VkApi api = new VkApi();
+        public static void Token()
+        {
+            api.Authorize(new ApiAuthParams
+            {
+                AccessToken = "access_token"
+            });
+        }
+        public static void Messg()
+        {
+            api.Messages.Send(new VkNet.Model.RequestParams.MessagesSendParams
+            {
+                PeerId = 0,
+                Message = "а"
+            });
+        }
+    }
+}
--- Heute/MainWindow.xaml.cs
+++ Heute/MainWindow.xaml.cs
@@ -1,38 +1,25 @@
 ﻿using System.Windows;
-using VkNet;
-using VkNet.Model;
 using System.Windows.Input;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Heute
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
             ToggleSwitch.TurnOff();
+            APIwk.Token();
         }
-        private void Token()
-        {
-            var api = new VkApi();
-            api.Authorize(new ApiAuthParams
-            {
-                AccessToken = ""
-            });
-        }
-
-        // Работа слайдера
+        
+        // Работа свитча
         private async void Switch_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
 
             if (!this.ToggleSwitch.Toggled)
             {
-                //this._cancellationGetUpdates?.Cancel();
                 return;
             }
 
@@ -42,27 +29,14 @@ namespace Heute
                 {                    
                     while (true)
                     {
-
-                    Thread.Sleep(1000);
-                        
-                    if ((System.DateTime.Now.Hour == 17) && (System.DateTime.Now.Minute == 42) && (System.DateTime.Now.Second == 00))
+                    Thread.Sleep(1000);                   
+                    if ((System.DateTime.Now.Hour == 00) && (System.DateTime.Now.Minute == 00) && (System.DateTime.Now.Second == 00))
                     {
-                            // отправка сообщения
-                            var api = new VkApi();
-                            api.Messages.Send(new VkNet.Model.RequestParams.MessagesSendParams
-                            {
-                                PeerId = 112853315,
-                                Message = "СИВОДНЯ" //пример сообщения
-                            });
+                            APIwk.Messg(); // Отправка сообщения
                         }
                 }
                 });
             }
-        }
-
-        private void Switch_Loaded(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
